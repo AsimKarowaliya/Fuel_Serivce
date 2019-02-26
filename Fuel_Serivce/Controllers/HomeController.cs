5399f191e69e4b9cfc024f72aa303592fa94c882
Asim K <akarowaliya@hotmail.com>
2019-02-26 13:37:09 -0600

    added Main

--- Fuel_Serivce/Controllers/HomeController.cs
+++ Fuel_Serivce/Controllers/HomeController.cs
@@ -25,6 +25,11 @@ namespace Fuel_Serivce.Controllers
             return View();
         }
 
+        public IActionResult Main()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
