e5131f9c12ec9f7500bafd85cb18771a6897da3f
Asim K <akarowaliya@hotmail.com>
2019-02-28 23:32:10 -0600

    added fuel quote history and little change to validation in the form

--- Fuel_Serivce/Controllers/HomeController.cs
+++ Fuel_Serivce/Controllers/HomeController.cs
@@ -36,6 +36,11 @@ namespace Fuel_Serivce.Controllers
             return View();
         }
 
+        public IActionResult Fuel_History()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
